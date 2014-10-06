b4e6cd0f54f4f8d452b2efba2e8d7cc070f89853
blakeparkinson <blake@edmodo.com>
2014-10-05 21:48:21 -0700

    sweeper stuff

---
+++ minesweeper/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  minesweeper
+//
+//  Created by Blake Parkinson on 10/5/14.
+//  Copyright (c) 2014 blakeparkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var boardView: UIView!
+    @IBOutlet weak var movesLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
