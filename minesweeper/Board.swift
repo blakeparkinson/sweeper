cebf3d53aee563242ea31afda0bde3ed5dae5d64
blakeparkinson <blake@edmodo.com>
2014-10-06 22:48:53 -0700

    more board

--- minesweeper/Board.swift
+++ minesweeper/Board.swift
@@ -13,5 +13,14 @@ class Board {
     var squares:[[Square]] = [] // a 2d array of square cells, indexed by [row][column]
     init(size:Int) {
         self.size = size
+        
+        for row in 0 ..< size {
+            var squareRow:[Square] = []
+            for col in 0 ..< size {
+                let square = Square(row: row, col: col)
+                squareRow.append(square)
+            }
+            squares.append(squareRow)
+        }
     }
 }
