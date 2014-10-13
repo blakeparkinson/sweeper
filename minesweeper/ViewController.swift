97be6fcd70e6d454a96fe5db83adf37e82ab5eb9
blakeparkinson <blake@edmodo.com>
2014-10-12 23:28:25 -0700

    resettign and starting

--- minesweeper/ViewController.swift
+++ minesweeper/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var boardView: UIView!
+    @IBOutlet weak var movesLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
     let BOARD_SIZE:Int = 10
     var board:Board
     var squareButtons:[SquareButton] = []
+    
     
     required init(coder aDecoder: NSCoder)
     {
@@ -20,13 +25,45 @@ class ViewController: UIViewController {
         super.init(coder: aDecoder)
     }
     
-    @IBOutlet weak var boardView: UIView!
-    @IBOutlet weak var movesLabel: UILabel!
-    @IBOutlet weak var timeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func initializeBoard() {
+        for row in 0 ..< board.size {
+            for col in 0 ..< board.size {
+                
+                let square = board.squares[row][col]
+                
+                let squareSize:CGFloat = self.boardView.frame.width / CGFloat(BOARD_SIZE)
+                
+                let squareButton = SquareButton(squareModel: square, squareSize: squareSize);
+                squareButton.setTitle("[x]", forState: .Normal)
+                squareButton.setTitleColor(UIColor.darkGrayColor(), forState: .Normal)
+                squareButton.addTarget(self, action: "squareButtonPressed:", forControlEvents: .TouchUpInside)
+                self.boardView.addSubview(squareButton)
+                
+                self.squareButtons.append(squareButton)
+            }
+        }
+    }
+    
+    func resetBoard() {
+        // resets the board with new mine locations & sets isRevealed to false for each square
+        self.board.resetBoard()
+        // iterates through each button and resets the text to the default value
+        for squareButton in self.squareButtons {
+            squareButton.setTitle("[x]", forState: .Normal)
+        }
+    }
+    
+    func startNewGame() {
+        //start new game
+        self.resetBoard()
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
